4b5fac08d9357fe1252ce83d53e90a293e697ceb
Emmanuel <Kelechizobam82@hmail.com>
2024-09-22 14:50:02 +0100

    add all

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PlayersList from "./Components/PlayeList";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1 style={{ textAlign: "center" }}>Player Cards</h1>
+      <PlayersList />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/players.js
@@ -0,0 +1,53 @@
+// src/players.js
+const players = [
+  {
+    name: "Lionel Messi",
+    team: "Inter Miami CF",
+    nationality: "Argentinian",
+    jerseyNumber: 10,
+    age: 36,
+    imageUrl: "https://example.com/messi.jpg",
+  },
+  {
+    name: "Cristiano Ronaldo",
+    team: "Al Nassr",
+    nationality: "Portuguese",
+    jerseyNumber: 7,
+    age: 39,
+    imageUrl: "https://example.com/ronaldo.jpg",
+  },
+  {
+    name: "Mbappe",
+    team: "Real Madrid",
+    nationality: "French",
+    jerseyNumber: 7,
+    age: 25,
+    imageUrl: "https://example.com/mbappe.jpg",
+  },
+  {
+    name: " Haaland",
+    team: "Manchester City",
+    nationality: "Norwegian",
+    jerseyNumber: 9,
+    age: 23,
+    imageUrl: "https://example.com/haaland.jpg",
+  },
+  {
+    name: "Saka",
+    team: "Arsenal",
+    nationality: "England",
+    jerseyNumber: 7,
+    age: 23,
+    imageUrl: "https://example.com/saka.jpg",
+  },
+  {
+    name: "Lamine Yamal",
+    team: "Barcelona",
+    nationality: "Spain",
+    jerseyNumber: 19,
+    age: 17,
+    imageUrl: "https://example.com/yamal.jpg",
+  },
+];
+
+export default players;
